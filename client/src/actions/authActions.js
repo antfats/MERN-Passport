e4a1e833ea8fd0b5eb902b87f578c06baac9da6a
bob <antfats@hotmail.com>
2019-07-05 09:28:09 -0700

    Finished authentication/get token/registar user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -19,10 +19,53 @@ export const registerUser = (userData, history) => dispatch => {
 
 //Login - get user token
 
-export const loginUser = userData => dispatch =>{
+export const loginUser = userData => dispatch => {
     axios
-    .post("/api/users/login",userData)
-    .then(res => {
-        
-    })
-}
+        .post("/api/users/login", userData)
+        .then(res => {
+
+
+            const { token } = res.data;
+            localStorage.setItem("jwtToken", token);
+
+            setAuthToken(Token);
+
+            const decoded = jwt_decode(token);
+
+            dispatch(setCurrentUser(decoded));
+        })
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
+
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    };
+};
+
+export const setUserLoading = () => {
+    return {
+        type: USER_LOADING
+    };
+};
+
+export const logoutUser = () => dispatch => {
+    localStorage.removeItem("jwtToken");
+    setAuthToken(false);
+
+    dispatch(setCurrentUser({}));
+};
+
+
+
+
+
+
+
+
