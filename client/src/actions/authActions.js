7181b60ebe662ddc01d808ea771de56d8deee423
bob <antfats@hotmail.com>
2019-07-05 09:19:38 -0700

    Start of MERN passport

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
+
+//registar user
+export const registerUser = (userData, history) => dispatch => {
+    axios
+        .post("/api/users/register", userData)
+        .then(res => history.push("/login"))
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
+
+//Login - get user token
+
+export const loginUser = userData => dispatch =>{
+    axios
+    .post("/api/users/login",userData)
+    .then(res => {
+        
+    })
+}
